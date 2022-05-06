4f76b9a46e64ebaaf535f69594102452e645bfd4
Ashwin Senthilvasan <asenthilvasan011@student.fuhsd.org>
2022-05-05 20:08:33 -0700

    alpha v1

---
+++ CapstoneProject/src/screens/SecondScreen.java
@@ -0,0 +1,88 @@
+package screens;
+
+
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import core.DrawingSurface;
+import sprites.Mario;
+import sprites.Sprite;
+
+
+public class SecondScreen extends Screen {
+	
+	private DrawingSurface surface;
+	
+	private Rectangle screenRect;
+
+	private Mario mario;
+	private List<Sprite> obstacles;
+
+	public SecondScreen(DrawingSurface surface) {
+		super(800,600);
+		this.surface = surface;
+		screenRect = new Rectangle(0,0,DRAWING_WIDTH,DRAWING_HEIGHT);
+		obstacles = new ArrayList<Sprite>();
+		obstacles.add(new Sprite(0,250,100,50));
+		obstacles.add(new Sprite(700,250,100,50));
+		obstacles.add(new Sprite(200,400,400,50));
+		obstacles.add(new Sprite(375,300,50,100));
+		obstacles.add(new Sprite(300,250,200,50));
+	}
+
+
+	public void spawnNewMario() {
+		mario = new Mario(surface.loadImage("img/mario.png"), DRAWING_WIDTH/2-Mario.MARIO_WIDTH/2,50);
+	}
+
+	// The statements in the setup() function 
+	// execute once when the program begins
+	public void setup() {
+		spawnNewMario();
+	}
+
+	// The statements in draw() are executed until the 
+	// program is stopped. Each statement is executed in 
+	// sequence and after the last line is read, the first 
+	// line is executed again.
+	public void draw() {
+		
+		// drawing stuff
+		
+		surface.background(0,255,255);   
+
+		for (Sprite s : obstacles) {
+			s.draw(surface);
+		}
+
+		mario.draw(surface);
+
+		
+		// modifying stuff
+
+		if (surface.isPressed(KeyEvent.VK_ESCAPE)) {
+			surface.switchScreen(ScreenSwitcher.MENU_SCREEN);
+			return;
+		}
+		if (surface.isPressed(KeyEvent.VK_LEFT))
+			mario.walk(Mario.Direction.Left);
+		if (surface.isPressed(KeyEvent.VK_RIGHT))
+			mario.walk(Mario.Direction.Right);
+		if (surface.isPressed(KeyEvent.VK_UP))
+			mario.walk(Mario.Direction.Up);
+		if (surface.isPressed(KeyEvent.VK_DOWN))
+			mario.walk(Mario.Direction.Down);
+		
+
+		mario.act(obstacles);
+
+		if (!screenRect.intersects(mario))
+			spawnNewMario();
+
+	}
+
+	
+}
---
+++ CapstoneProject/src/sprites/Mario.java
@@ -0,0 +1,123 @@
+package sprites;
+
+
+import java.awt.geom.Line2D;
+import java.awt.geom.Line2D.Double;
+import java.util.List;
+
+import processing.core.PImage;
+
+public class Mario extends Sprite {
+
+	public static final int MARIO_WIDTH = 40;
+	public static final int MARIO_HEIGHT = 60;
+	
+	private boolean canDown, canUp, canRight, canLeft;
+
+	public enum Direction {
+		Up,Down,Left,Right
+	}
+	
+	public Mario(PImage img, int x, int y) {
+		super(img, x, y, MARIO_WIDTH, MARIO_HEIGHT);
+	}
+
+	// METHODS
+	public void walk(Direction dir) {
+		if (dir == Direction.Left) {
+			moveByAmount(-5,0);
+		}
+		if (dir == Direction.Right) {
+			moveByAmount(5,0);
+		}
+		if (dir == Direction.Down) {
+			moveByAmount(0,5);
+		}
+		if (dir == Direction.Up) {
+			moveByAmount(0,-5);
+		}
+	}
+
+	public void jump() {
+		
+	}
+
+	public void act(List<Sprite> obstacles) {
+
+		 for (Sprite s : obstacles) {
+			 if (super.intersects(s)) {
+				 boolean vertical = false, horizontal = false;
+				 Line2D[] chosenLine = new Line2D[2];
+				 // check if we intersect with sprites box
+				 if (super.intersectsLine(s.x, s.y, s.x + s.width, s.y)) { // top horiz
+					 //System.out.println("top horiz");
+
+					 horizontal = true;
+					 chosenLine[0] = new Line2D.Double(s.x, s.y, s.x + s.width, s.y);
+				 }
+				 else if (super.intersectsLine(s.x, s.y + s.height, s.x + s.width, s.y + s.height)) { // bottom horiz
+					 //System.out.println("bottom horiz");
+
+					 horizontal = true;
+					 chosenLine[0] = new Line2D.Double(s.x, s.y + s.height, s.x + s.width, s.y + s.height);
+				 }
+				 if (super.intersectsLine(s.x, s.y, s.x, s.y + s.height)) { // left vert
+					// System.out.println("left vert");
+
+					 vertical = true;
+					 chosenLine[1] = new Line2D.Double(s.x, s.y, s.x, s.y + s.height);
+				 }
+				 else if (super.intersectsLine(s.x + s.width, s.y, s.x + s.width, s.y + s.height)) { // right vert
+					// System.out.println("right vert");
+
+					 vertical = true;
+					 chosenLine[1] = new Line2D.Double(s.x + s.width, s.y, s.x + s.width, s.y + s.height);
+				 }
+	
+				 if (vertical && horizontal) {
+					 //vertical intersection
+					 double interVert = 0, interHoriz = 0;
+					 if (this.y < s.y) {
+						 interVert = chosenLine[0].getY1() - (this.y + this.height);
+					 } else {
+						 interVert = chosenLine[0].getY1() - (this.y);
+					 }
+					 
+					 if (this.x < s.x) {
+						 interHoriz = chosenLine[1].getX1() - (this.x + this.width);
+					 } else {
+						 interHoriz = chosenLine[1].getX1() - this.x;
+					 }
+					 
+					 if (Math.abs(interHoriz) < Math.abs(interHoriz)) {
+						 this.moveByAmount(0, interVert);
+					 } else {
+						 this.moveByAmount(interHoriz, 0);
+					 }
+					 
+				 } 
+				 else if (vertical) {
+					 if (this.x < s.x) { //left side
+						 this.moveToLocation(s.x-this.width, this.y);
+					 } else if (this.x > s.x) {
+						 this.moveToLocation(s.x + s.width, this.y);
+					 } else {
+						 //System.out.println("x is equal to sprite x");
+					 }
+					 
+				 } else {
+					 if (this.y < s.y) {
+						 this.moveToLocation(this.x, s.y - this.height);
+					 }
+					 else if (this.y > s.y) {
+						 this.moveToLocation(this.x, s.y + s.height);
+					 } else {
+						 //System.out.println("y is equal to sprite y");
+					 }
+				 }
+			 }
+		 }
+	}
+
+
+}
