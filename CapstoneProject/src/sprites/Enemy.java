18dd12e78b025429324a53d00ed720cd06f82ce6
asenthilvasan <67446655+asenthilvasan@users.noreply.github.com>
2022-05-13 09:30:18 -0700

    small enemy changes

--- CapstoneProject/src/sprites/Enemy.java
+++ CapstoneProject/src/sprites/Enemy.java
@@ -11,7 +11,9 @@ public class Enemy extends Sprite{
 	
 	//Weapon weapon;
 	
-	int enemySpeed;
+	int enemySpeed, attackPower;
+	Player player;
+	
 	
 	
 	/**
@@ -23,9 +25,11 @@ public class Enemy extends Sprite{
 	 * @param h The height of the Enemy.
 	 * @param speed The speed that the Enemy moves.
 	 */
-	public Enemy(PImage img, int x, int y, int w, int h, int speed) {
+	public Enemy(PImage img, int x, int y, int w, int h, int speed, int attackPower, Player player) {
 		super(img, x, y, w, h);
 		enemySpeed = speed;
+		this.attackPower = attackPower;
+		this.player = player;
 	} 
 	
 	/**
@@ -33,7 +37,7 @@ public class Enemy extends Sprite{
 	 * @param player The Player that the method calculates the distance from.
 	 * @return The distance from this Enemy to the Player.
 	 */
-	private double distanceFromPlayer(Player player) {
+	private double distanceFromPlayer() {
 		double result = Math.abs(player.x - this.x);
 		return result;
 	}
@@ -50,8 +54,8 @@ public class Enemy extends Sprite{
 	 * Moves this Enemy towards the Player.
 	 * @param player
 	 */
-	public void moveTowardsPlayer(Player player) {
-		if (distanceFromPlayer(player) > 20) {
+	public void moveTowardsPlayer() {
+		if (distanceFromPlayer() > 20) {
 			if (player.x > this.x) {
 				this.moveByAmount(enemySpeed, 0);
 			} else {
@@ -64,9 +68,9 @@ public class Enemy extends Sprite{
 	 * Continuously moves towards the Player and attacks if close enough.
 	 * @param player
 	 */
-	public void act(Player player) {
-		moveTowardsPlayer(player);
-		if (distanceFromPlayer(player) <= 20) {
+	public void act() {
+		moveTowardsPlayer();
+		if (distanceFromPlayer() <= 20) {
 			attack();
 		}
 	}
