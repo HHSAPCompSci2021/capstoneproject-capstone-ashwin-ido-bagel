5abb70fdfbe789ab96ec784b2d4d8cfd657462da
Ashwin Senthilvasan <asenthilvasan011@student.fuhsd.org>
2022-05-18 12:04:55 -0700

    starting implementation of multiple backgrounds

--- CapstoneProject/src/screens/SecondScreen.java
+++ CapstoneProject/src/screens/SecondScreen.java
@@ -5,10 +5,14 @@ import java.awt.Rectangle;
 import processing.core.PGraphics;
 import java.awt.event.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import processing.core.PImage;
 
 import core.DrawingSurface;
+import sprites.Enemy;
 import sprites.NPC;
 import sprites.Player;
 import sprites.Sprite;
@@ -33,8 +37,13 @@ public class SecondScreen extends Screen {
 
 	private Player player;
 	private List<Sprite> obstacles;
-	private NPC npc;
-
+	private NPC npc, enemy;
+	private Map<PImage, List<Sprite>> backgrounds = new HashMap<PImage, List<Sprite>>();
+	// trying to make it so that when character walks through a 'door', a new background loads up
+	// with all obstacles in it and characters loaded up
+	// problem: need to separate obstacles that get drawn first and npcs, enemies that get drawn later
+	// possible solution: make two entries into map for each background-> kind of annoying and prone to manual errors
+	
 	/**
 	 * Creates a game screen that has a drawing surface, and can have obstacles and a player.
 	 * 
@@ -81,9 +90,12 @@ public class SecondScreen extends Screen {
 	public void setup() {
 		background = surface.loadImage("img/background.png");
 		spawnNewPlayer();
-		npc = new NPC(surface.loadImage("img/Enemy.png"), 300, 290);
+		npc = new NPC(surface.loadImage("img/npc1.png"), 300, 290); //make sprite transparent
+		enemy = new NPC(surface.loadImage("img/Enemy.png"), 450, 290);
 		obstacles.add(new Sprite((int)npc.x+5, (int)npc.y+5, (int)npc.width-10, (int)npc.height-15));
-		npc.setText("Press P to fight boss.");
+		obstacles.add(new Sprite((int)enemy.x+5, (int)enemy.y+5, (int)enemy.width-10, (int)enemy.height-15));
+		npc.setText("Welcome to the game. Have fun in your journey!");
+		enemy.setText("Fight me!");
 	}
 
 	
@@ -112,6 +124,7 @@ public class SecondScreen extends Screen {
 
 		player.draw(surface);
 		npc.draw(surface);
+		enemy.draw(surface);
 		surface.fill(0);
 		surface.textSize(14);
 		surface.text("Click on me!", (float)npc.x, (float)npc.y - 5);
@@ -154,12 +167,17 @@ public class SecondScreen extends Screen {
 	}
 
 	/**
-	 * Checks if the npc is clicked, if it is it prompts the user.
+	 * Checks if a character is clicked and performs an action.
 	 * 
 	 */
 	public void mousePressed() { 
 		if(npc.contains(surface.mouseX, surface.mouseY) && npc.intersects(player)) {
 			npc.displayText();
+		}
+		if (enemy.contains(surface.mouseX, surface.mouseY) && enemy.intersects(player)) {
+			enemy.displayText();
+			surface.switchScreen(ScreenSwitcher.BATTLE_SCREEN);
+			
 		}
 	}
 	
