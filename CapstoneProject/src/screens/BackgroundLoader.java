4301c2c368df479cb36fdb4e5bfdbf39c8b88c32
Ashwin Senthilvasan <asenthilvasan011@student.fuhsd.org>
2022-05-20 10:25:15 -0700

    small

--- CapstoneProject/src/screens/BackgroundLoader.java
+++ CapstoneProject/src/screens/BackgroundLoader.java
@@ -10,23 +10,20 @@ import sprites.Sprite;
 
 public class BackgroundLoader {
 	
-	List<Sprite> characters, obstacles;
-	PImage background;
-	//int screenNum;
+	ArrayList<ArrayList<Sprite>> characters, obstacles;
+	ArrayList<PImage> backgrounds;
+	int screenNum;
 	
-	public BackgroundLoader(PImage background, List<Sprite> characters, List<Sprite> obstacles) {
-		this.background = background;
-		this.characters = characters;
-		this.obstacles = obstacles;
-		//this.screenNum = screenNum;
+	public BackgroundLoader(int screenNum) {
+		this.screenNum = screenNum;
 	}
 	
 	public void draw(PApplet surface, Screen s) {
-		for (Sprite a : obstacles) {
+		for (Sprite a : obstacles.get(screenNum)) {
 			a.draw(surface);
 		}
-		surface.image(background, 0, 0, s.DRAWING_WIDTH,s.DRAWING_HEIGHT);
-		for (Sprite b : characters) {
+		surface.image(backgrounds.get(screenNum), 0, 0, s.DRAWING_WIDTH,s.DRAWING_HEIGHT);
+		for (Sprite b : characters.get(screenNum)) {
 			b.draw(surface);
 		}
 		
